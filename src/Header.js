57cc111c84e909bd68fb8259ebacf01132a20232
unknown <afeezbabatunde@gmail.com>
2020-07-16 14:14:57 +0100

    NavBar component completed

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import logo from "./img/myzone.png";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
-        <nav>
+        <nav className="header">
+            <Link to="/">
+                <img className="header__logo"
+                src={logo}
+                alt=""
+                />
+            </Link>
+            {/* Search Input*/}
+            <div className="hearch__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            {/* 3 Links */}
+            <div className="header__nav">
+                {/* 1st Link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header_optionLineOne">Hello Tunde</span>
+                        <span className="header_optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+                {/* 2nd Link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header_optionLineOne">Returns</span>
+                        <span className="header_optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                {/* 3rd Link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header_optionLineOne">Your</span>
+                        <span className="header_optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+                {/* 4th Link */}
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        {/* Shopping Basket Icon */}
+                        <ShoppingBasketIcon />
+                        {/* Shopping Basket Icon */}
+                        <span className="header_optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+
+            {/* Basket Icon and number */}
             
         </nav>
     )
